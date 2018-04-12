163a4999c7369afee508bc1b4af64129df9eb706
Tyler Lindow <tyler.lindow@gmail.com>
2018-04-11 22:43:31 -0700

    beautify (atom package)

--- js/app.js
+++ js/app.js
@@ -16,16 +16,16 @@ const stars = document.querySelectorAll(".fa-star");
 // declaring variable of matchedCards
 let matchedCard = document.getElementsByClassName("match");
 
- // stars list
- let starsList = document.querySelectorAll(".stars li");
+// stars list
+let starsList = document.querySelectorAll(".stars li");
 
- // close icon in modal
- //let closeicon = document.querySelector(".close");
+// close icon in modal
+//let closeicon = document.querySelector(".close");
 
- // declare modal
- let modal = document.getElementById("popup1")
+// declare modal
+let modal = document.getElementById("popup1")
 
- // array for opened cards
+// array for opened cards
 var openedCards = [];
 var matchCount = 0;
 
@@ -34,17 +34,18 @@ var matchCount = 0;
 // @param {array}
 // @returns shuffledarray
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 };
 
 
@@ -53,204 +54,205 @@ document.body.onload = startGame();
 
 
 // @description function to start a new play
-function startGame(){
-    // shuffle deck
-    cards = shuffle(cards);
-    // remove all exisiting classes from each card
-    for (var i = 0; i < cards.length; i++){
-        deck.innerHTML = "";
-        [].forEach.call(cards, function(item) {
-            deck.appendChild(item);
-        });
-        cards[i].classList.remove("show", "open", "match", "disabled");
-    }
-    // reset moves
-    moves = 40;
-    counter.innerHTML = moves;
-    // reset rating
-    for (var i= 0; i < stars.length; i++){
-        stars[i].style.color = "#FFD700";
-        stars[i].style.visibility = "visible";
-    }
-    //reset timer
-    second = 59;
-    minute = 1;
-    var timer = document.querySelector(".timer");
-    timer.innerHTML = "1 min 59 secs";
-    clearInterval(interval);
+function startGame() {
+  // shuffle deck
+  cards = shuffle(cards);
+  // remove all exisiting classes from each card
+  for (var i = 0; i < cards.length; i++) {
+    deck.innerHTML = "";
+    [].forEach.call(cards, function(item) {
+      deck.appendChild(item);
+    });
+    cards[i].classList.remove("show", "open", "match", "disabled");
+  }
+  // reset moves
+  moves = 40;
+  counter.innerHTML = moves;
+  // reset rating
+  for (var i = 0; i < stars.length; i++) {
+    stars[i].style.color = "#FFD700";
+    stars[i].style.visibility = "visible";
+  }
+  //reset timer
+  second = 59;
+  minute = 1;
+  var timer = document.querySelector(".timer");
+  timer.innerHTML = "1 min 59 secs";
+  clearInterval(interval);
 }
 
 
 // @description toggles open and show class to display cards
-var displayCard = function (){
-    this.classList.toggle("open");
-    this.classList.toggle("show");
-    this.classList.toggle("disabled");
+var displayCard = function() {
+  this.classList.toggle("open");
+  this.classList.toggle("show");
+  this.classList.toggle("disabled");
 };
 
 
 // @description add opened cards to OpenedCards list and check if cards are match or not
 function cardOpen() {
-    openedCards.push(this);
-    var len = openedCards.length;
-    console.log(len);
-    moveCounter();
-    if(len === 2){
-        //moveCounter();
-        if(openedCards[0].type === openedCards[1].type){
-            setTimeout(matched, 1200);
-        } else {
-            unmatched();
-        }
+  openedCards.push(this);
+  var len = openedCards.length;
+  console.log(len);
+  moveCounter();
+  if (len === 2) {
+    //moveCounter();
+    if (openedCards[0].type === openedCards[1].type) {
+      setTimeout(matched, 1200);
+    } else {
+      unmatched();
     }
+  }
 };
 
 
 // @description when cards match
-function matched(){
-    openedCards[0].classList.add("match", "disabled");
-    openedCards[1].classList.add("match", "disabled");
-    openedCards[0].classList.remove("show", "open", "no-event");
-    openedCards[1].classList.remove("show", "open", "no-event");
-    congratulations();
-    openedCards = [];
+function matched() {
+  openedCards[0].classList.add("match", "disabled");
+  openedCards[1].classList.add("match", "disabled");
+  openedCards[0].classList.remove("show", "open", "no-event");
+  openedCards[1].classList.remove("show", "open", "no-event");
+  congratulations();
+  openedCards = [];
 }
 
 
 // description when cards don't match
-function unmatched(){
-    openedCards[0].classList.add("unmatched");
-    openedCards[1].classList.add("unmatched");
-    disable();
-    setTimeout(function(){
-        openedCards[0].classList.remove("show", "open", "no-event","unmatched");
-        openedCards[1].classList.remove("show", "open", "no-event","unmatched");
-        enable();
-        openedCards = [];
-    },2200);
+function unmatched() {
+  openedCards[0].classList.add("unmatched");
+  openedCards[1].classList.add("unmatched");
+  disable();
+  setTimeout(function() {
+    openedCards[0].classList.remove("show", "open", "no-event", "unmatched");
+    openedCards[1].classList.remove("show", "open", "no-event", "unmatched");
+    enable();
+    openedCards = [];
+  }, 2200);
 }
 
 
 // @description disable cards temporarily
-function disable(){
-    Array.prototype.filter.call(cards, function(card){
-        card.classList.add('disabled');
-    });
+function disable() {
+  Array.prototype.filter.call(cards, function(card) {
+    card.classList.add('disabled');
+  });
 }
 
 
 // @description enable cards and disable matched cards
-function enable(){
-    Array.prototype.filter.call(cards, function(card){
-        card.classList.remove('disabled');
-        for(var i = 0; i < matchedCard.length; i++){
-            matchedCard[i].classList.add("disabled");
-        }
-    });
+function enable() {
+  Array.prototype.filter.call(cards, function(card) {
+    card.classList.remove('disabled');
+    for (var i = 0; i < matchedCard.length; i++) {
+      matchedCard[i].classList.add("disabled");
+    }
+  });
 }
 
 
 // @description count player's moves
-function moveCounter(){
-    moves--;
-    counter.innerHTML = moves;
-    //start timer on first click
-    if(moves == 39){
-        second = 59;
-        minute = 1;
-        startTimer();
-    }
-    // setting rates based on moves (not in use)
-    // if (moves > 8 && moves < 12){
-    //     for( i= 0; i < 3; i++){
-    //         if(i > 1){
-    //             stars[i].style.visibility = "collapse";
-    //         }
-    //     }
-    // }
-    // else if (moves > 13){
-    //     for( i= 0; i < 3; i++){
-    //         if(i > 0){
-    //             stars[i].style.visibility = "collapse";
-    //         }
-    //     }
-    // }
+function moveCounter() {
+  moves--;
+  counter.innerHTML = moves;
+  //start timer on first click
+  if (moves == 39) {
+    second = 59;
+    minute = 1;
+    startTimer();
+  }
+  // setting rates based on moves (not in use)
+  // if (moves > 8 && moves < 12){
+  //     for( i= 0; i < 3; i++){
+  //         if(i > 1){
+  //             stars[i].style.visibility = "collapse";
+  //         }
+  //     }
+  // }
+  // else if (moves > 13){
+  //     for( i= 0; i < 3; i++){
+  //         if(i > 0){
+  //             stars[i].style.visibility = "collapse";
+  //         }
+  //     }
+  // }
 }
 
 
 // @description game timer
-var second = 59, minute = 1;
+var second = 59,
+  minute = 1;
 var timer = document.querySelector(".timer");
 var interval;
-function startTimer(){
-    interval = setInterval(function(){
-        timer.innerHTML = minute+"mins "+second+"secs";
-        if (minute ==0 && second ==0){
-          clearInterval(interval);
-        } else if (minute==1 && second==0){
-          second=59;
-          minute--;
-        } else if (minute == 0 && second == 59) {
-          document.querySelector(".timer").style.color = "orange";
-        } else if (second == 29 && minute == 0) {
-            document.querySelector(".timer").style.color = "red";
-        }
-        second--;
-    },1000);
+
+function startTimer() {
+  interval = setInterval(function() {
+    timer.innerHTML = minute + "mins " + second + "secs";
+    if (minute == 0 && second == 0) {
+      clearInterval(interval);
+    } else if (minute == 1 && second == 0) {
+      second = 59;
+      minute--;
+    } else if (minute == 0 && second == 59) {
+      document.querySelector(".timer").style.color = "orange";
+    } else if (second == 29 && minute == 0) {
+      document.querySelector(".timer").style.color = "red";
+    }
+    second--;
+  }, 1000);
 }
 
-
 // @description congratulations when all cards match, show modal and moves, time and rating
-function congratulations(){
-        clearInterval(interval);
-        //var finalTime = timer.innerHTML;
+function congratulations() {
+  clearInterval(interval);
+  //var finalTime = timer.innerHTML;
 
-        // show congratulations modal
-        modal.classList.add("show");
+  // show congratulations modal
+  modal.classList.add("show");
 
-        // declare star rating variable
-        //var starRating = document.querySelector(".stars").innerHTML;
+  // declare star rating variable
+  //var starRating = document.querySelector(".stars").innerHTML;
 
 
-        //showing move, rating, time on modal
-        //document.getElementById("finalMove").innerHTML = moves;
-        //document.getElementById("starRating").innerHTML = starRating;
-        //document.getElementById("totalTime").innerHTML = finalTime;
+  //showing move, rating, time on modal
+  //document.getElementById("finalMove").innerHTML = moves;
+  //document.getElementById("starRating").innerHTML = starRating;
+  //document.getElementById("totalTime").innerHTML = finalTime;
 
-        if (openedCards[0].type == "leaf") {
-          document.getElementById("factOne").innerHTML = "Biodiversity helps us research new solutions to problems. Biodiversity has helped us engineer new organs for patients and it has helped up improve our nutrition.";
-          document.getElementById("myImage").src = "img/nutrition.jpg";
-        } else if (openedCards[0].type == "anchor") {
-          document.getElementById("factOne").innerHTML = "Phytoplankton form the base of virtually every ocean food web. In short, they make most other ocean life possible.";
-          document.getElementById("myImage").src = "img/phytoplankton.jpg";
-        } else if (openedCards[0].type == "plane") {
-          document.getElementById("factOne").innerHTML = "Nothing to see here!";
-          document.getElementById("myImage").src = "img/worms.jpg";
-        } else if (openedCards[0].type == "bolt") {
-          document.getElementById("factOne").innerHTML = "The outbreaks of widespread diseases, such as SARS, Ebola, and malaria, are a result of changes in biodiversity.";
-          document.getElementById("myImage").src = "img/ebola.jpg";
-        } else if (openedCards[0].type == "cube") {
-          document.getElementById("factOne").innerHTML = "On the coast, coral reefs can limit damage from storms and tidal waves. Coral reefs act as a physical barrier to help protect our ecosystems!";
-          document.getElementById("myImage").src = "img/coral_reef.jpg";
-        } else if (openedCards[0].type == "bicycle") {
-          document.getElementById("factOne").innerHTML = "Hiking, kayaking, and bicycling keep us healthy. They’re fun too! There would be fewer places to go and less to see without biodiversity!";
-          document.getElementById("myImage").src = "img/hiking.jpg";
-        } else if (openedCards[0].type == "diamond") {
-          document.getElementById("factOne").innerHTML = "There can be 10,000 to 50,000 species in less than a teaspoon of soil. There are more microbes in a teaspoon of soil than there are people on the earth!";
-          document.getElementById("myImage").src = "img/soil.jpg";
-        } else if (openedCards[0].type == "bomb") {
-          document.getElementById("factOne").innerHTML = "Urban trees help us convert carbon dioxide to oxygen through photosynthesis. They also filter pollutants in our air."
-          document.getElementById("myImage").src = "img/urban_trees.jpg";
-        }
-        setInterval(interval);
-        //closeicon on modal
+  if (openedCards[0].type == "leaf") {
+    document.getElementById("factOne").innerHTML = "Biodiversity helps us research new solutions to problems. Biodiversity has helped us engineer new organs for patients and it has helped up improve our nutrition.";
+    document.getElementById("myImage").src = "img/nutrition.jpg";
+  } else if (openedCards[0].type == "anchor") {
+    document.getElementById("factOne").innerHTML = "Phytoplankton form the base of virtually every ocean food web. In short, they make most other ocean life possible.";
+    document.getElementById("myImage").src = "img/phytoplankton.jpg";
+  } else if (openedCards[0].type == "plane") {
+    document.getElementById("factOne").innerHTML = "Nothing to see here!";
+    document.getElementById("myImage").src = "img/worms.jpg";
+  } else if (openedCards[0].type == "bolt") {
+    document.getElementById("factOne").innerHTML = "The outbreaks of widespread diseases, such as SARS, Ebola, and malaria, are a result of changes in biodiversity.";
+    document.getElementById("myImage").src = "img/ebola.jpg";
+  } else if (openedCards[0].type == "cube") {
+    document.getElementById("factOne").innerHTML = "On the coast, coral reefs can limit damage from storms and tidal waves. Coral reefs act as a physical barrier to help protect our ecosystems!";
+    document.getElementById("myImage").src = "img/coral_reef.jpg";
+  } else if (openedCards[0].type == "bicycle") {
+    document.getElementById("factOne").innerHTML = "Hiking, kayaking, and bicycling keep us healthy. They’re fun too! There would be fewer places to go and less to see without biodiversity!";
+    document.getElementById("myImage").src = "img/hiking.jpg";
+  } else if (openedCards[0].type == "diamond") {
+    document.getElementById("factOne").innerHTML = "There can be 10,000 to 50,000 species in less than a teaspoon of soil. There are more microbes in a teaspoon of soil than there are people on the earth!";
+    document.getElementById("myImage").src = "img/soil.jpg";
+  } else if (openedCards[0].type == "bomb") {
+    document.getElementById("factOne").innerHTML = "Urban trees help us convert carbon dioxide to oxygen through photosynthesis. They also filter pollutants in our air."
+    document.getElementById("myImage").src = "img/urban_trees.jpg";
+  }
+  setInterval(interval);
+  //closeicon on modal
 }
 
 
 // @description close icon on modal
-function closeModal(){
-    modal.classList.remove("show");
-    startTimer();
+function closeModal() {
+  modal.classList.remove("show");
+  startTimer();
 }
 
 // Will use later to create different levels of difficulty
@@ -262,9 +264,9 @@ function closeModal(){
 // }
 
 // loop to add event listeners to each card
-for (var i = 0; i < cards.length; i++){
-    card = cards[i];
-    card.addEventListener("click", displayCard);
-    card.addEventListener("click", cardOpen);
-    //card.addEventListener("click",congratulations);
+for (var i = 0; i < cards.length; i++) {
+  card = cards[i];
+  card.addEventListener("click", displayCard);
+  card.addEventListener("click", cardOpen);
+  //card.addEventListener("click",congratulations);
 };
